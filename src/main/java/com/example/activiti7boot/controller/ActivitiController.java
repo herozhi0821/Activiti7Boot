7a51db7239a03d735ec20d616de952d447869796
herozhi0821 <herozhi0821@163.com>
2021-07-07 18:01:07 +0800

    update

--- src/main/java/com/example/activiti7boot/controller/ActivitiController.java
+++ src/main/java/com/example/activiti7boot/controller/ActivitiController.java
@@ -29,7 +29,7 @@ public class ActivitiController {
         Map<String, Object> map = new HashMap<String, Object>();
         //在holiday.bpmn中,填写请假单的任务办理人为动态传入的userId,此处模拟一个id
         map.put("userId", 2);
-    	ProcessInstance instance = runtimeService.startProcessInstanceByKey("Process_1mr4ugq",map);
+    	ProcessInstance instance = runtimeService.startProcessInstanceByKey("kyrsapplication",map);
 		return instance;
     }
 
